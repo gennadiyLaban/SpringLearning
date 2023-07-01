d3f6cbce78335de89e804cdc6f209d4bdee312c1
gennadiyLaban <gena.laban@gmail.com>
2023-07-01 21:52:47 +0300

    LESSON-4-BOOKSHOP setup new frontend

--- bookShop/src/main/java/org/laban/learning/spring/bookshop/controllers/MainPageController.java
+++ bookShop/src/main/java/org/laban/learning/spring/bookshop/controllers/MainPageController.java
@@ -1,12 +1,15 @@
 package org.laban.learning.spring.bookshop.controllers;
 
+import java.util.List;
+
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.laban.learning.spring.bookshop.data.book.Book;
 import org.laban.learning.spring.bookshop.services.book.BookService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
@@ -18,9 +21,12 @@ public class MainPageController {
     @NonNull private final BookService bookService;
 
     @GetMapping(value = { "/", "/index.html", "/main", "/main/index.html" })
-    public String mainPage(Model model) {
-        log.info("GET main page");
-        model.addAttribute("bookData", bookService.getBookData());
+    public String mainPage() {
         return VIEW_MAIN;
+    }
+
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks() {
+        return bookService.getBooksData();
     }
 }
