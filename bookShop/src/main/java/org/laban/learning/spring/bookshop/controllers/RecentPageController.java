45545ddb1e1b1123551d8152ebc1475e736ef31d
gennadiyLaban <gena.laban@gmail.com>
2023-07-04 21:10:32 +0300

    LESSON-4-BOOKSHOP unify header and footer

--- bookShop/src/main/java/org/laban/learning/spring/bookshop/controllers/RecentPageController.java
+++ bookShop/src/main/java/org/laban/learning/spring/bookshop/controllers/RecentPageController.java
@@ -1,18 +1,30 @@
 package org.laban.learning.spring.bookshop.controllers;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.laban.learning.spring.bookshop.data.book.Book;
+import org.laban.learning.spring.bookshop.services.book.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class RecentPageController {
+    private final BookService bookService;
+
     @GetMapping(value = { "/books/recent", "/books/recent.html" })
     public String recent() {
         log.info("GET %s".formatted(Pages.RECENT.baseUrl));
         return "books/recent";
+    }
+
+    @ModelAttribute("recentBooks")
+    public List<Book> recentBooks() {
+        return bookService.getBooksData();
     }
 
 }
