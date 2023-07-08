2fb7d630303d61f01a82cdd996dcf66869d4c3ec
gennadiyLaban <gena.laban@gmail.com>
2023-07-08 13:08:45 +0300

    LESSON-4-BOOKSHOP resolve contacts page localisation

--- bookShop/src/main/java/org/laban/learning/spring/bookshop/controllers/ContactsPageController.java
+++ bookShop/src/main/java/org/laban/learning/spring/bookshop/controllers/ContactsPageController.java
@@ -1,9 +1,13 @@
 package org.laban.learning.spring.bookshop.controllers;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 @Slf4j
@@ -13,5 +17,13 @@ public class ContactsPageController {
     public String contacts() {
         log.info("GET %s".formatted(Pages.CONTACTS.baseUrl));
         return "contacts";
+    }
+
+    @ModelAttribute("navigationPath")
+    public Map<Pages, String> navigationPath() {
+        var path = new LinkedHashMap<Pages, String>();
+        path.put(Pages.MAIN, "str.fragment.breadcrumbs.path.main");
+        path.put(Pages.CONTACTS, "str.fragment.breadcrumbs.path.contacts");
+        return path;
     }
 }
