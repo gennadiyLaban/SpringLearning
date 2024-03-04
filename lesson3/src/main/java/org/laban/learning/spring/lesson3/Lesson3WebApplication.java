d43cd575aad08000b251d24fdcb364e2f0907dfa
tagudur <tagudur@yandex.ru>
2024-03-04 12:27:08 +0300

    LESSON-3 init commit

---
+++ lesson3/src/main/java/org/laban/learning/spring/lesson3/Lesson3WebApplication.java
@@ -0,0 +1,13 @@
+package org.laban.learning.spring.lesson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson3WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson3WebApplication.class, args);
+    }
+
+}
