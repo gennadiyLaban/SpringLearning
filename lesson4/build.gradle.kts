b49ff4fd233e475c585d1c4de9141b4fccf5f61a
tagudur <tagudur@yandex.ru>
2024-03-14 18:20:24 +0300

    LESSON-4 init commit

---
+++ lesson4/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    applyPlugin(Global.Plugin.KotlinJVM)
+    applyPlugin(Global.Plugin.SpringBoot)
+    applyPlugin(Global.Plugin.SpringDependencyManagement)
+}
+
+group = "org.laban"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = Global.Version.JAVA_VERSION
+}
+
+dependencies {
+    implementation(Global.Library.SpringBootStarter)
+    implementation(Global.Library.SpringBootWebStarter)
+    implementation(Global.Library.SpringBootThymeleafStarter)
+    implementation(Global.Library.SpringBootValidationStarter)
+//    implementation(Global.Library.SpringBootDataJdbcStarter)
+//    runtimeOnly(Global.Library.Postgresql)
+
+    annotationProcessor(Global.Library.SpringBootConfigurationProcessor)
+
+    compileOnly(Global.Library.Lombok)
+    annotationProcessor(Global.Library.Lombok)
+
+    implementation(Global.Library.ApacheCommonLang3)
+
+    developmentOnly(Global.Library.SpringBootDevTools)
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ lesson4/src/main/java/org/laban/learning/spring/lesson4/Lesson4RestApplication.java
@@ -0,0 +1,11 @@
+package org.laban.learning.spring.lesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4RestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson4RestApplication.class, args);
+    }
+}
