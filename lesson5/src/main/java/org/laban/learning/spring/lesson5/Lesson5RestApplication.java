48bfe42f830fa94fcde64a63d1deb4fe653f58a6
tagudur <tagudur@yandex.ru>
2024-03-27 14:49:06 +0300

    LESSON-5 init commit

---
+++ lesson5/src/main/java/org/laban/learning/spring/lesson5/Lesson5RestApplication.java
@@ -0,0 +1,11 @@
+package org.laban.learning.spring.lesson5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson5RestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson5RestApplication.class, args);
+    }
+}
---
+++ lesson5/src/main/resources/db/init.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS lesson5;
+ALTER ROLE admin IN DATABASE lesson5 SET SEARCH_PATH = lesson5,public;
+SET SEARCH_PATH = lesson5,public;
