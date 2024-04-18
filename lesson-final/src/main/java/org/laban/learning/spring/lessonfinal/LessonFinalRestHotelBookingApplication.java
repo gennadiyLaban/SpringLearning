fb4d387ea84b31c12155a2ddfc87a384e9e2af7c
tagudur <tagudur@yandex.ru>
2024-04-18 18:42:13 +0300

    LESSON-FINAL init commit

---
+++ lesson-final/src/main/java/org/laban/learning/spring/lessonfinal/LessonFinalRestHotelBookingApplication.java
@@ -0,0 +1,11 @@
+package org.laban.learning.spring.lessonfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonFinalRestHotelBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LessonFinalRestHotelBookingApplication.class, args);
+    }
+}
---
+++ lesson-final/src/main/resources/db/init.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS lessonfinal;
+ALTER ROLE admin IN DATABASE lessonfinal SET SEARCH_PATH=lessonfinal,public;
+SET SEARCH_PATH=lessonfinal,public;
