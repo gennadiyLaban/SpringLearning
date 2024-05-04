ad1f9802bfba2edf3a4503b66e2b5830cf66cb49
Laban Gennadiy <gena.laban@gmail.com>
2024-05-04 23:14:02 +0300

    LESSON-FINAL resolve api/v1/booking/** endpoints protection

--- lesson-final/src/main/java/org/laban/learning/spring/lessonfinal/web/controller/BookingController.java
+++ lesson-final/src/main/java/org/laban/learning/spring/lessonfinal/web/controller/BookingController.java
@@ -6,6 +6,7 @@ import org.laban.learning.spring.lessonfinal.web.dto.booking.BookingDTO;
 import org.laban.learning.spring.lessonfinal.web.dto.booking.BookingListDTO;
 import org.laban.learning.spring.lessonfinal.web.validation.group.ValidationGroup;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +18,13 @@ import java.net.URI;
 public class BookingController {
     private final BookingService bookingService;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<BookingDTO> getBookingById(@PathVariable Long id) {
         return ResponseEntity.ok(bookingService.getBookingDtoById(id));
     }
 
+    @PreAuthorize("hasAnyRole('USER')")
     @PostMapping
     public ResponseEntity<Void> createBooking(
             @RequestBody
@@ -32,6 +35,7 @@ public class BookingController {
         return ResponseEntity.created(URI.create("/api/v1/booking/" + createdId)).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/hotel/{hotelId}/list")
     public ResponseEntity<BookingListDTO> getAllHotelBookings(
             @PathVariable("hotelId") Long hotelId
@@ -39,6 +43,7 @@ public class BookingController {
         return ResponseEntity.ok(bookingService.getBookingListDTOForHotel(hotelId));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/user/{userId}/list")
     public ResponseEntity<BookingListDTO> getAllUserBookings(
             @PathVariable("userId") Long userId
